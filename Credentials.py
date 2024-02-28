448dd2265e8f75e54a27d76523657e32037cb01c
jiteshsugandh <88026182+jiteshsugandh@users.noreply.github.com>
2024-02-28 18:16:57 +0530

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -1,8 +1,8 @@
-access_token = 'd119c67f-51ea-30df-8478-77c3669fbb7c'
-userid = 'JITESH98'
-consumer_key = 'lv1VA6Oqo3uRgvDFAfD0oOn3SYka'
-app_id = 'fc1yP6PanxZehCclqNqLxc0b2lka'
-password = 'Jitesh@123'
-ip = "127.0.0.1"
+access_token = ''
+userid = ''
+consumer_key = ''
+app_id = ''
+password = ''
+ip = ""
 fDataFrame = "" #FNO data
 cDataFrame = "" #Cash data
